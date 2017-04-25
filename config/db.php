fe8f33fc6d64b8819a76cd1ca234dd2b52b834f7
liujinxing <1136261505@qq.com>
2017-04-25 11:43:11 +0800

    liujx update config db.php

--- config/db.php
+++ config/db.php
@@ -1,8 +1,7 @@
 <?php
-
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=examination',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=examination',
     'username' => 'root',
     'password' => 'gongyan',
     'charset' => 'utf8',
