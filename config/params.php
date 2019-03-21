0d5fc6fbccce8687f86cb4426dbdbdb0e8ed354e
liujinxing <jinxing.liu@verystar.cn>
2019-03-21 10:23:47 +0800

    添加代码格式化配置

--- config/params.php
+++ config/params.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'adminRoleName' => 'administrator',     // 超级管理员角色名
-    'adminEmail' => 'admin@example.com',
+    'adminRoleName'                 => 'administrator',     // 超级管理员角色名
+    'adminEmail'                    => 'admin@example.com',
     'user.passwordResetTokenExpire' => 3600,
-    'cacheTime' => 86400,                  // 用户登录的缓存时间
-    'status' => ['停用', '启用'],        // 通用状态
+    'cacheTime'                     => 86400,                  // 用户登录的缓存时间
+    'status'                        => ['停用', '启用'],        // 通用状态
 ];
