e4d423a24983e7f19325f4f120b2ca73a9ed1f50
liujinxing <jingxin.liu@qq.com>
2019-03-21 10:48:21 +0800

    delete config

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'class'       => 'yii\db\Connection',
-    'dsn'         => 'mysql:host=localhost;dbname=examination',
-    'username'    => 'user',
-    'password'    => 'Ljx&19930813',
-    'charset'     => 'utf8',
-    'tablePrefix' => 'ks_',
-];
