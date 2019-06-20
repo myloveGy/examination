23eeb9722ccc11c16c1af5a5965a5acc8a2adc73
刘金星 <821901008@qq.com>
2019-06-20 23:31:19 +0800

    feat: 修改项目信息

--- config/params.php
+++ config/params.php
@@ -6,4 +6,7 @@ return [
     'user.passwordResetTokenExpire' => 3600,
     'cacheTime'                     => 86400,                  // 用户登录的缓存时间
     'status'                        => ['停用', '启用'],        // 通用状态
+    'projectName'                   => '考试系统',
+    'projectTitle'                  => '考试系统',
+    'companyName'                   => '<span class="blue bolder"> Liujinxing </span> 考试系统 &copy; 2016-2018',
 ];
