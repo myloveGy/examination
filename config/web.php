5ed40b5f59dfdc4e93140407d0fca35e2bded6ad
jinxing.liu <821901008@qq.com>
2019-06-21 18:20:15 +0800

    refactor: json 返回修改

--- config/web.php
+++ config/web.php
@@ -16,10 +16,10 @@ $config = [
                     'class'          => 'jinxing\admin\Module',
                     'user'           => 'admin',
                     'leftTopButtons' => [],
-                    'userLinks'      => []
-                ]
+                    'userLinks'      => [],
+                ],
             ],
-            'layout'  => '@jinxing/admin/views/layouts/main'
+            'layout'  => '@jinxing/admin/views/layouts/main',
         ],
     ],
     'components' => [
@@ -96,13 +96,24 @@ $config = [
             'bundles' => [
                 // 去掉自己的bootstrap 资源
                 'yii\bootstrap\BootstrapAsset' => [
-                    'css' => []
+                    'css' => [],
                 ],
 
                 // 去掉自己加载的Jquery
                 'yii\web\JqueryAsset'          => [
                     'sourcePath' => null,
                     'js'         => [],
+                ],
+            ],
+        ],
+
+        // 配置语言
+        'i18n'         => [
+            'translations' => [
+                'admin' => [
+                    'class'          => 'yii\i18n\PhpMessageSource',
+                    'sourceLanguage' => 'en',
+                    'basePath'       => '@jinxing/admin/messages',
                 ],
             ],
         ],
