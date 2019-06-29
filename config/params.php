54f61a91e7932c6100baec7558f412020d058a74
刘金星 <821901008@qq.com>
2019-06-29 16:43:22 +0800

    docs: 文档更新

--- config/params.php
+++ config/params.php
@@ -4,9 +4,9 @@ return [
     'adminRoleName'                 => 'administrator',     // 超级管理员角色名
     'adminEmail'                    => 'admin@example.com',
     'user.passwordResetTokenExpire' => 3600,
-    'cacheTime'                     => 86400,                  // 用户登录的缓存时间
-    'status'                        => ['停用', '启用'],        // 通用状态
-    'projectName'                   => '考试系统',
-    'projectTitle'                  => '考试系统',
-    'companyName'                   => '<span class="blue bolder"> Liujinxing </span> 考试系统 &copy; 2016-2018',
+
+    'projectOpenOther' => true,
+    'projectName'      => '考试系统',
+    'projectTitle'     => '考试系统',
+    'companyName'      => '<span class="blue bolder"> Liujinxing </span> 考试系统 &copy; 2016-2018',
 ];
